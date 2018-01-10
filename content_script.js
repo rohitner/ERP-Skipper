44e4b26b9ea8e471c662b02976a5cbe01ce17417
rohitner <rohitner1@gmail.com>
2018-01-10 12:13:32 +0530

    added local files

---
+++ content_script.js
@@ -0,0 +1,47 @@
+// function skip() {
+//     console.log($("#skiplink"));
+//
+//     $("#skiplink").click();
+//
+// }
+
+// alert("something");
+
+// window.setTimeout(function() {
+// console.log(document.getElementById("skiplink"));
+//   console.log("1");
+//
+// },1000);
+//
+// window.setTimeout(function() {
+//   console.log(document.getElementById("skiplink"));
+//
+//   console.log("2");
+// },2000);
+//
+// window.setTimeout(function() {
+//   console.log(document.getElementById("skiplink"));
+//
+//   console.log("3");
+//
+// },3000);
+
+
+window.setTimeout(function() {
+    console.log(console.log(document.getElementById("skiplink")));
+    console.log("dibya");
+    document.getElementById("skiplink").click();
+
+},1000);
+
+
+// $("document").ready(function() {
+//     console.log($("#skiplink"));
+//     console.log("dibya");
+//     $("#skiplink").click();
+//
+// });
+
+
+// $("#skiplink").click();
+//skip();
